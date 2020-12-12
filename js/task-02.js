00c5cc2eaa4f26753b3b2cfb62a5875fd170dd82
DrAnastasiia <dr.anastasiyya0@gmail.com>
2020-12-12 20:21:43 +0200

    Task 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,4 +1,14 @@
 // Перепиши функцию toggleUserState() так, чтобы она не использовала callback-функцию callback, а принимала всего два параметра allUsers и userName и возвращала промис.
+//-----УСЛОВИЕ
+// const toggleUserState = (allUsers, userName, callback) => {
+//   const updatedUsers = allUsers.map(user =>
+//     user.name === userName ? { ...user, active: !user.active } : user,
+//   );
+// };
+//   callback(updatedUsers);
+
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
 
 const users = [
   { name: 'Mango', active: true },
@@ -7,24 +17,16 @@ const users = [
   { name: 'Lux', active: false },
 ];
 
-const toggleUserState = (allUsers, userName, callback) => {
-  const updatedUsers = allUsers.map(user =>
-    user.name === userName ? { ...user, active: !user.active } : user,
-  );
-
-  callback(updatedUsers);
+const toggleUserState = (allUsers, userName) => {
+  return new Promise(resolve => {
+    const updatedUsers = allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user,
+    );
+    resolve(updatedUsers);
+  });
 };
 
 const logger = updatedUsers => console.table(updatedUsers);
 
-/*
- * Сейчас работает так
- */
-toggleUserState(users, 'Mango', logger);
-toggleUserState(users, 'Lux', logger);
-
-/*
- * Должно работать так
- */
 toggleUserState(users, 'Mango').then(logger);
 toggleUserState(users, 'Lux').then(logger);
